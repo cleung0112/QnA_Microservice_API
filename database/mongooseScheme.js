f5dc64d62d3e514154aff8829cad4f8b35a3e42c
cleung0112 <cleung0112@gmail.com>
2021-03-16 16:15:52 -0700

    Add referencing approach

--- database/mongooseScheme.js
+++ database/mongooseScheme.js
@@ -1,34 +1,69 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const ProductQuestionsScheme = new Schema({
+const ProductQnASchema = new Schema({
   product_id: { type: Number, autoIndex: true, unique:true },
-  results: [question],
+  results: [questionSchema],
 });
 
-const question = new Schema({
-  question_id: { type: Number, autoIndex: true },
+const questionSchema = new Schema({
+  _id: { type: Number, autoIndex: true },
   question_body: { type: String, required: true },
-  question_date: { type: Date, required: true },
-  asker_name: { type: String, required: true, unique:true },
+  question_date: { type: Date, required: true, default: Date },
+  asker_name: { type: String, required: true },
   question_helpfulness: { type: Number, default: 0 },
   reported: { type: Boolean, default: false },
-  answers: [answer]
+  answers: [answerSchema]
 });
 
-const answer = new Schema({
-  id: { type: Number, autoIndex: true },
+const answerSchema = new Schema({
+  _id: { type: Number, autoIndex: true },
   body: { type: String, required: true },
-  date: { type: Date, required: true },
-  answerer_name: { type: String, required: true, unique:true },
+  date: { type: Date, required: true, default: Date },
+  answerer_name: { type: String, required: true },
   helpfulness: { type: Number, default: 0 },
   report: { type: Boolean, default: false },
-  photos: [photo]
+  photos: [photosSchema]
 });
 
-const photos = new Schema({
+const photosSchema  = new Schema({
   photoURL: String
 });
 
-// const ProductQuestions = db.model('ProductQuestions',ProductQuestionsScheme);
+const ProductQnA = db.model('ProductQuestions',ProductQnASchema);
 // module.exports = ProductQuestions
+
+// referencing approach
+
+const ProductQnASchema = new Schema({
+  product_id: { type: Number, autoIndex: true, unique:true },
+  results: [questionSchema]
+});
+
+const questionSchema = new Schema({
+  question_body: { type: String, required: true },
+  question_date: { type: Date, required: true, default: Date },
+  asker_name: { type: String, required: true },
+  question_helpfulness: { type: Number, default: 0 },
+  reported: { type: Boolean, default: false },
+  answers: { type: mongoose.Schema.Types.ObjectId,
+    ref: 'Answers'}
+});
+
+const answerSchema = new Schema({
+  body: { type: String, required: true },
+  date: { type: Date, required: true, default: Date },
+  answerer_name: { type: String, required: true },
+  helpfulness: { type: Number, default: 0 },
+  report: { type: Boolean, default: false },
+  photos: { type: mongoose.Schema.Types.ObjectId,
+    ref: 'Photos'}
+});
+
+const Answers = db.model('Answers',answerSchema);
+
+const photosSchema  = new Schema({
+  photoURL: String
+});
+
+const photos = db.model('Photos',photosSchema);
--- database/sqlScheme.js
+++ database/sqlScheme.js
@@ -5,10 +5,10 @@ CREATE TABLE `Questions` (
   `id` INTEGER NOT NULL AUTO_INCREMENT,
   `questionBody` VARCHAR(255) NOT NULL,
   `helpfulness` SMALLINT DEFAULT 0 COMMENT 'Assumption: total votes on helpfulness < 32767',
-  `reported` TINYINT DEFAULT 0,
+  `reported` SMALLINT DEFAULT 0,
   `userID` INTEGER NOT NULL,
   `productID` INTEGER NOT NULL,
-  `askedTime` TIMESTAMP NOT NULL COMMENT 'Assumption: web app services customer from different time zo',
+  `askedTime` TIMESTAMP NOT NULL COMMENT 'Assumption: web app services customer from different time zone',
   PRIMARY KEY (`id`),
   FOREIGN KEY (userID) REFERENCES `Users` (`id`),
   FOREIGN KEY (productID) REFERENCES `product` (`id`)
@@ -19,7 +19,7 @@ DROP TABLE IF EXISTS `product`;
 
 CREATE TABLE `product` (
   `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `product_ID` CHAR(5) NOT NULL,
+  `product_ID` INTEGER NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`product_ID`)
 );
@@ -42,7 +42,7 @@ CREATE TABLE `Answers` (
   `id` INTEGER NOT NULL AUTO_INCREMENT,
   `answerBody` VARCHAR(500) NOT NULL,
   `helpfulness` SMALLINT DEFAULT 0,
-  `reported` TINYINT DEFAULT 0,
+  `reported` SMALLINT DEFAULT 0,
   `answerTime` TIMESTAMP NOT NULL,
   `userID` INTEGER NOT NULL,
   PRIMARY KEY (`id`),
